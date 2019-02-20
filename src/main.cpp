3238505ca986ec5e081ba3b36434f85178e1bfa5
sixdaycoder <sixdaycoder@gmail.com>
2019-02-20 16:02:48 +0800

    Add TableCell Template

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,23 @@
 #include <string>
 #include <iostream>
+#include <stdio.h>
 #include "table/table_record.h"
+#include "table/table_cell.h"
 using namespace std;
 
 int main()
 {
+    TableCellInt32 cellInt32(500);
+    printf("Int32 : %d Type : %d\n", cellInt32.GetElem(), cellInt32.GetType());
 
-    cout << "Init Empty Project" << endl;
+    TableCellInt64 cellInt64(3123123123123123123);
+    printf("Int64 : %ld Type : %d\n", cellInt64.GetElem(), cellInt64.GetType());
+
+    TableCellString cellString("hello world");
+    printf("string : %s Type : %d\n", cellString.GetElem(), cellString.GetType());
+
+    TableCellReal cellReal(31.23231f);
+    printf("Float : %f Type : %d\n", cellReal.GetElem(), cellReal.GetType());
+
     return 0;
 }
---
+++ src/table/table_cell.h
@@ -0,0 +1,109 @@
+//
+// Created by Administrator on 2019/2/20.
+//
+
+#ifndef TABLE2CODE_TABLE_CELL_H
+#define TABLE2CODE_TABLE_CELL_H
+
+
+#include <assert.h>
+#include <stdint.h>
+#include <string>
+
+class TableCellType
+{
+public:
+    enum
+    {
+        TABLE_CELL_TYPE_INT32 = 0,//int32
+        TABLE_CELL_TYPE_INT64 = 1,//int64
+        TABLE_CELL_TYPE_STRING = 2,//string
+        TABLE_CELL_TYPE_REAL = 3,//float
+    };
+
+public:
+
+    static int32_t Int32;
+    static int64_t Int64;
+    static std::string String;
+    static float   Float;
+};
+
+int32_t TableCellType::Int32 = 0;
+int64_t TableCellType::Int64 = 0;
+std::string TableCellType::String;
+float TableCellType::Float = 0.0f;
+
+
+template<typename ElementT, int Type>
+class TableCell
+{
+public:
+
+    explicit TableCell(const ElementT& elem)
+    {
+        m_Element = elem;
+        assert(TypeCheck());
+    }
+
+    ~TableCell() = default;
+
+public:
+
+    int GetType() const { return Type; }
+
+    const ElementT& GetElem() const { return m_Element; }
+
+private:
+
+    ElementT m_Element;
+
+    bool TypeCheck()
+    {
+        switch(Type)
+        {
+            case TableCellType::TABLE_CELL_TYPE_INT32:
+                return typeid(m_Element) == typeid(TableCellType::Int32);
+            case TableCellType::TABLE_CELL_TYPE_INT64:
+                return typeid(m_Element) == typeid(TableCellType::Int64);
+            case TableCellType::TABLE_CELL_TYPE_STRING:
+                return typeid(m_Element) == typeid(TableCellType::String);
+            case TableCellType::TABLE_CELL_TYPE_REAL:
+                return typeid(m_Element) == typeid(TableCellType::Float);
+            default:
+                break;
+        }
+        return false;
+    }
+};
+
+template<>
+class TableCell<std::string, TableCellType::TABLE_CELL_TYPE_STRING>
+{
+public:
+
+    explicit TableCell(const std::string& elem)
+    {
+        m_Element = elem;
+    }
+
+    ~TableCell() = default;
+
+public:
+
+    int GetType() const { return TableCellType::TABLE_CELL_TYPE_STRING; }
+
+    const char* GetElem() const { return m_Element.c_str(); }
+
+private:
+
+    std::string m_Element;
+};
+
+
+using TableCellInt32 = TableCell<int32_t, TableCellType::TABLE_CELL_TYPE_INT32>;
+using TableCellInt64 = TableCell<int64_t, TableCellType::TABLE_CELL_TYPE_INT64>;
+using TableCellString = TableCell<std::string, TableCellType::TABLE_CELL_TYPE_STRING>;
+using TableCellReal = TableCell<float, TableCellType::TABLE_CELL_TYPE_REAL>;
+
+#endif //TABLE2CODE_TABLE_CELL_H
